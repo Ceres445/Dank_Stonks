559b363f7fcbab6d9333e3200c0d3f03d67eca52
Ceres <ddruva.boomer@gmail.com>
2021-01-13 17:08:02 +0530

    forgot a line

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -24,7 +24,8 @@ async def is_staff(ctx):
     await guild.get_data()
     if guild.staff is None:
         raise GuildNotSetup(f" The guild {guild.guild.name} is not setup. Setup with {guild.prefix[0]}setup")
-    if not guild.
+    if not guild.verified:
+        return False
     if set([role.id for role in ctx.author.roles]) & set(guild.staff):
         return True
     else:
